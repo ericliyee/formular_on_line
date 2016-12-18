f4d40ec349cc885dfb7ad6acc43de93360f37022
ericleel <ericliyee@126.com>
2016-12-18 09:45:25 +0800

    12.18

--- main.py
+++ main.py
@@ -6,13 +6,21 @@ from __init__ import *
 class MainHandler(web.RequestHandler):
     @gen.coroutine
     def get(self):
-        self.render('index.html')
+        timus = session.query(timu).all()
+        self.render('main.html',timus=timus)
 
-
+class EditTimu(web.RequestHandler):
+    @gen.coroutine
+    def get(self):
+        timu_id = self.get_argument('timu_id')
+        timu = session.query(timu).filter(timu.id==timu_id)
+        self.render('timuEdit.html',timu=timu)
+        
 settings={
     'static_path' :os.path.join(os.path.dirname(__file__),'static'),
     'template_path' :os.path.join(os.path.dirname(__file__),'templates'),
-    'debug':True
+    'debug':True,
+    'autoescape':False
     }
         
 def make_app():
@@ -23,5 +31,5 @@ def make_app():
 
 if __name__=='__main__':
     app = make_app()
-    app.listen('8888')
+    app.listen('8000')
     ioloop.IOLoop.instance().start()
